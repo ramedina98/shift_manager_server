f13cb600b95e3dcb1ce4eca802f923dcf139980d
ramedina98 <rmedinamartindelcampo@gmail.com>
2025-01-06 16:06:10 -0600

    feat: redis for railway

--- src/config/redisLock.ts
+++ src/config/redisLock.ts
@@ -1,6 +1,10 @@
 import Redis from "ioredis";
 
-const redis = new Redis();
+const redis = new Redis({
+    host: process.env.REDIS_HOST || "127.0.0.1", // Host de Redis
+    port: Number(process.env.REDIS_PORT) || 6379, // Puerto de Redis
+    password: process.env.REDIS_PASSWORD || undefined, // Contraseña de Redis (si la hay)
+});
 
 /**
  * Acquire a lock in Redis.
